ad2f55bdba6e9d57de02aeb88b881df1f2a099c2
yuvalhef <41970107+yuvalhef@users.noreply.github.com>
2020-02-11 16:34:56 +0200

    log pipelines

--- gym_deepline/envs/primitives/ensemble.py
+++ gym_deepline/envs/primitives/ensemble.py
@@ -236,7 +236,7 @@ class XGBClassifierMetaPrim(primitive):
         super(XGBClassifierMetaPrim, self).__init__(name='XGBClassifierMeta')
         self.id = 78
         self.hyperparams = []
-        self.type = 'Classifier'
+        self.type = 'ensemble'
         self.description = "XGBoost is an optimized distributed gradient boosting library designed to be highly efficient, flexible and portable. It implements machine learning algorithms under the Gradient Boosting framework. XGBoost provides a parallel tree boosting (also known as GBDT, GBM) that solve many data science problems in a fast and accurate way. The same code runs on major distributed environment (Hadoop, SGE, MPI) and can solve problems beyond billions of examples."
         self.hyperparams_run = {'default': True}
         self.random_state = random_state
